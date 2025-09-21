5fbcc809e49ee43d346fa7939484e294e4cf744e
Матюшкина Мария <maria18112005@yandex.ru>
2025-09-21 19:52:35 +0700

    Стили

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, url_for, request, redirect
 import datetime
+
 app = Flask(__name__)
+
 
 @app.errorhandler(404)
 def not_found(err):
     return "нет такой страницы", 404
 
+
 @app.route("/web")
 def web():
-    return """<!doctype html>
+    return (
+        """<!doctype html>
         <html>
             <body>
                 <h1>web-сервер на flask</h1>
                 <a href="/author">author</a>
             <body>
-        </html>""", 200, {
-            'X-Server': 'sample',
-            'Content-Type': 'text/plain; charset=utf-8'
-        }
+        </html>""",
+        200,
+        {"X-Server": "sample", "Content-Type": "text/plain; charset=utf-8"},
+    )
+
 
 @app.route("/author")
 def author():
@@ -25,32 +30,52 @@ def author():
     group = "ФБИ-32"
     faculty = "ФБ"
 
-    return """<!doctype html>
+    return (
+        """<!doctype html>
         <html>
            <body>
-                <p>Студент: """ + name + """</p>
-                <p>Группа: """ + group + """</p>
-                <p>Факультет: """ + faculty + """</p>
+                <p>Студент: """
+        + name
+        + """</p>
+                <p>Группа: """
+        + group
+        + """</p>
+                <p>Факультет: """
+        + faculty
+        + """</p>
                 <a href="/web">web</a>
             <body>
         </html>"""
-   
-@app.route('/image')
+    )
+
+
+@app.route("/image")
 def image():
     path = url_for("static", filename="картинка.jpg")
-    return '''
-    <!doctype html> 
-    <html>
-        <body>
-            <h1>Природа</h1>
-            <img src="''' + path + '''">
-        </body>
-    </html>
-    '''
+    css_path = url_for("static", filename="lab1.css")
+    return (
+        f'''
+<!doctype html> 
+<html>
+    <head>
+        <title>Природа</title>
+        <link rel="stylesheet" type="text/css" href="{css_path}">
+    </head>
+    <body>
+        <h1>Природа</h1>
+        <img src="'''
+        + path
+        + """">
+    </body>
+</html>
+"""
+    )
+
 
 count = 0
 
-@app.route('/counter')
+
+@app.route("/counter")
 def counter():
     global count
     count += 1
@@ -58,24 +83,39 @@ def counter():
     url = request.url
     client_ip = request.remote_addr
 
-    return '''
+    return (
+        """
 <!doctype html> 
 <html>
     <body>
-        Сколько раз вы сюда заходили: ''' + str(count) + '''
+        Сколько раз вы сюда заходили: """
+        + str(count)
+        + """
         <hr>
-        Дата и время: ''' + str(time) + '''<br>
-        запрошенный адрес: ''' + url + '''<br>
-        Ваш IP-адрес: ''' + client_ip + '''<br>
+        Дата и время: """
+        + str(time)
+        + """<br>
+        запрошенный адрес: """
+        + url
+        + """<br>
+        Ваш IP-адрес: """
+        + client_ip
+        + """<br>
     </body>
 </html>
-'''    
+"""
+    )
+
+
 @app.route("/info")
 def info():
     return redirect("/author")
+
+
 @app.route("/created")
 def created():
-    return '''
+    return (
+        """
 <!doctype html> 
 <html>
     <body>
@@ -83,4 +123,6 @@ def created():
         <div><i> что то создано...</i></div>
     </body>
 </html>
-''', 201        
+""",
+        201,
+    )
