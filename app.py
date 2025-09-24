50111bd5ebceadd68af1402bfbf5d99c38471d8c
Матюшкина Мария <maria18112005@yandex.ru>
2025-09-24 17:15:01 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -323,3 +323,12 @@ def a():
 @app.route('/lab2/a/')
 def a2():
     return 'со слэшем'
+
+flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        abort(404)
+    else:
+        return "цветок: " + flower_list[flower_id]
