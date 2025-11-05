b18eaab88bbeb58ade61f7dcc06b71052202dcee
Матюшкина Мария <maria18112005@yandex.ru>
2025-11-05 18:34:44 +0700

    незакончено на паре

--- lab5.py
+++ lab5.py
@@ -6,5 +6,16 @@ lab5 = Blueprint('lab5',__name__)
 def lab():
     return render_template('lab5/lab5.html')
 
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
 
-    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login and password):
+        return render_template('lab5/register.html', error='Заполните все поля')
+
+
+
