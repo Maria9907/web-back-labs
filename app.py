7e81b9c7e065fb953a66c25ded073a7be54dab2c
Матюшкина Мария <maria18112005@yandex.ru>
2025-11-05 16:50:33 +0700

    Заготовка к 5-ой лабораторной

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 app = Flask(__name__)
 
 app.secret_key = 'секретно-секретный секрет'
@@ -20,7 +21,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
-
+app.register_blueprint(lab5)
 
 @app.errorhandler(400)
 def bad_request(err):
@@ -69,6 +70,9 @@ def index():
             </li>
             <li>
                 <a href="/lab4">Четвертая лабораторная работа</a>
+            </li>
+            <li>
+                <a href="/lab5">Пятая лабораторная работа</a>
             </li>
         </ul>
             
