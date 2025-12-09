7ea998caf0b5a3f06b1c08350cc8da35dda8a233
Матюшкина Мария <maria18112005@yandex.ru>
2025-12-09 22:20:21 +0700

    Заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 app = Flask(__name__)
 
 
@@ -30,6 +31,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 
 @app.errorhandler(400)
@@ -88,6 +90,9 @@ def index():
             </li>
             <li>
                 <a href="/lab7">Седьмая лабораторная работа</a>
+            </li>
+            <li>
+                <a href="/lab8">Восьмая лабораторная работа</a>
             </li>
         </ul>
             
